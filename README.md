555bf091fc930bcf752fddd4ed4c038f8a5b4c23
anheyman <antoineheymans@yahoo.fr>
2020-10-11 12:54:22 +0200

    update readme install instruction + handle error in analyser

--- README.md
+++ README.md
@@ -1,15 +1,53 @@
 ﻿# Topologie-Roman
 Etude de la topologie de réseaux d'acteurs extraits à partir de romans célèbres
+Ce projet permet la production de réseaux d'acteurs à partir de texte de roman ou de script.
 
-Cette version du projet tourne sur python 2.7 et demande les librairies suivantes: 
-* pattern
-* matplotlib
-* network 1.11
-* dill
-* scipy (only needed for auxillary)
+## Architecture
 
+### Code
+Le code est situé dans le répertoir Source_code_output/src
 Le programme comprends 3 types de fichier en plus du "main" central "bookAnalyser" :
 * les fichiers book* sont utilisés pour l'analyse des livres.
 * les fichiers mainX permettent de tester individuellement les fichiers bookX.
 * les fichiers aux* contiennent également une boucle principale et exécutent des fonctions externes à l'analyse principale. 
- 
+* le fichier bookAnalyser permet de lancer l'analyse complète de romans
+
+### Input/Output
+
+Tout les fichiers textes correctement formatés doivent être placés dans le répertoir Source_code_output/books_raw.
+Pour les scripts, des outils d'aide au formattage sont disponible dans Script_processing.
+Après que le programme général aie analysé un texte, le résultat du preprocessing sera stocké dans  Source_code_output/books_objects sous forme de fichiers books.
+Lors des analyses suivantes, le fichiers books du roman sera automatiquement récupéré, veillez donc à le supprimer si vous avez modifier le texte correspondant.
+Tout les autres output, csv graphs et images, sont situés dans plusieurs dossiers du répertoir Source_code_output.
+
+## Exécution
+
+### Installation du projet
+
+Afin de pouvoir exécuter le projet, les commandes suivantes doivent être exécutées:
+git clone https://github.com/antheymans/Topologie-Roman.git
+cd Topologie-Roman
+conda create --name topologie-roman --file spec-file.txt 
+conda activate topologie-roman
+cd Source_code_output/src
+
+### Exécution du code
+
+* python bookAnalyser.py permet de démarrer la boucle principale.
+Le programme demandera ensuite quel document .txt doit être lu. 
+L'utilisateur peut également répondre "all" pour démarrer l'analyse de tout les romans ou l'option "some" pour l'analyse des romans dont le preprocessing n'a pas été effectué.
+* python mainAux.py permet de générer la signature (c-a-d la production d'un certain nombre de métrique) de tout les réseaux produits.
+* différents fichiers main permettent d'exécuter des sous parties des analyses ou des aux. 
+* le fichier auxGenerateGraph.py permet de générer certains graphs relatifs à la distribution de degrés de ces réseaux.
+
+Cette version du projet tourne sur python 3.6 et demande notamment les librairies suivantes: 
+* matplotlib
+* network 1.11
+* dill
+* scipy (uniquement pour les fichiers "aux")
+* pattern
+
+Comme le projet pattern sur python 3 n'a pas abouti, une branche permettant d'utiliser les fonctions nécessaires 
+a été fournie à l'adresse:
+https://github.com/antheymans/pattern
+
--- Source_code_output/src/bookAnalyzer.py
+++ Source_code_output/src/bookAnalyzer.py
@@ -7,6 +7,8 @@ import bookDialogExtraction as bDE
 import bookCharacterIdentification as bCID
 import bookNetworkBuilding as bNB
 import time 
+import sys
+
 
 
 from helpers import PATH_BOOKS, compute_statistics, compute_statistics_CID
@@ -16,57 +18,59 @@ from helpers import PATH_BOOKS, compute_statistics, compute_statistics_CID
 ###############################################################
 
 def book_analyze(file_txt):
-    tmps1=time.time()
-    
-    """
-    DESCRIPTION: Main book analysis function
-    INPUT: Filename in the PATH_BOOKS folder
-    OUTPUT: .csv, .png and output files (no return statement) 
-    """
-    filename = file_txt[:-4]
+    try: 
+        tmps1=time.time()
+        """
+        DESCRIPTION: Main book analysis function
+        INPUT: Filename in the PATH_BOOKS folder
+        OUTPUT: .csv, .png and output files (no return statement) 
+        """
+        filename = file_txt[:-4]
 
-    print("COMPUTING BOOK : ", filename)
-    bIO.create_folders(filename)
-    sentences, breaks, sentiments, chunks, speakers, script = bIO.load_book(filename)
-    
-    # Extracts the dialogs from the book
-    print("Extracting dialogs...")
-    dialog_spacing, dialog_occurrences, dialog_contexts, count, threshold = bDE.dialog_extraction(sentences, breaks, sentiments, chunks, speakers)
-    print("Dialog extraction complete !")
+        print("COMPUTING BOOK : ", filename)
+        bIO.create_folders(filename)
+        sentences, breaks, sentiments, chunks, speakers, script = bIO.load_book(filename)
+        
+        # Extracts the dialogs from the book
+        print("Extracting dialogs...")
+        dialog_spacing, dialog_occurrences, dialog_contexts, count, threshold = bDE.dialog_extraction(sentences, breaks, sentiments, chunks, speakers)
+        print("Dialog extraction complete !")
 
-    #Build the CSV spacing map
-    bIO.build_csv_spacing_map(dialog_spacing, count, threshold, filename)
-    
-    #Print stats for character identification and output them to a file that is constantly updated.
-    bIO.save_occurrences_contexts(filename,dialog_occurrences,dialog_contexts)
-    stat = compute_statistics(dialog_occurrences)
-    bIO.update_stats(filename,stat)
-    
-    #Load the name files
-    previous_book = ""
-    if False:
-        previous_book = input("Use a previous book? If so, enter its name.")
-    oldAliasTable, oldConnectionsTable, oldAliases = bIO.load_alias_table(previous_book)
-    
-    print("Character analysis...")
-    aliasTable, connectionsTable, aliases = bCID.character_analysis(dialog_contexts, dialog_occurrences, chunks,oldAliasTable,oldConnectionsTable,oldAliases,speakers, script)
-    print("Character analysis complete !")
-    
-    bIO.export_aliases(aliasTable, connectionsTable, aliases, filename)
-    bIO.save_occurrences_CID(filename, dialog_occurrences)
-    
-    stat = compute_statistics_CID(dialog_occurrences)
-    bIO.update_stats_CID(filename,stat)
-    
+        #Build the CSV spacing map
+        bIO.build_csv_spacing_map(dialog_spacing, count, threshold, filename)
+        
+        #Print stats for character identification and output them to a file that is constantly updated.
+        bIO.save_occurrences_contexts(filename,dialog_occurrences,dialog_contexts)
+        stat = compute_statistics(dialog_occurrences)
+        bIO.update_stats(filename,stat)
+        
+        #Load the name files
+        previous_book = ""
+        if False:
+            previous_book = input("Use a previous book? If so, enter its name.")
+        oldAliasTable, oldConnectionsTable, oldAliases = bIO.load_alias_table(previous_book)
+        
+        print("Character analysis...")
+        aliasTable, connectionsTable, aliases = bCID.character_analysis(dialog_contexts, dialog_occurrences, chunks,oldAliasTable,oldConnectionsTable,oldAliases,speakers, script)
+        print("Character analysis complete !")
+        
+        bIO.export_aliases(aliasTable, connectionsTable, aliases, filename)
+        bIO.save_occurrences_CID(filename, dialog_occurrences)
+        
+        stat = compute_statistics_CID(dialog_occurrences)
+        bIO.update_stats_CID(filename,stat)
+        
 
-    # Network building
-    iGraphs, graphs = bNB.build_networks(dialog_occurrences, len(dialog_contexts), connectionsTable)
-    
-    bIO.output_graphs(filename, iGraphs, graphs)
-    
-    tmps2=time.time()-tmps1
-    print("Temps d'execution = %f" %tmps2)
-    print("Analysis complete!")
+        # Network building
+        iGraphs, graphs = bNB.build_networks(dialog_occurrences, len(dialog_contexts), connectionsTable)
+        
+        bIO.output_graphs(filename, iGraphs, graphs, False)
+        
+        tmps2=time.time()-tmps1
+        print("Temps d'execution = %f" %tmps2)
+        print("Analysis complete!")
+    except:
+        print("Oops!", sys.exc_info()[0], "of value ",sys.exc_info()[1]," occurred at ",sys.exc_info()[2]," while processing file", file_txt)
 
 def read_all():
     # Go through all the books
